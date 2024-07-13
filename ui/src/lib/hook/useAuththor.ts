1c1ec94576724928825fc6430fe4333180d870b9
Truong Hoang <hoangtrungtruongyt2020@gmail.com>
2024-07-13 21:36:30 +0700

    fix: useAuthor & add valid password in loginDto

--- ui/src/lib/hook/useAuththor.ts
+++ ui/src/lib/hook/useAuththor.ts
@@ -15,29 +15,20 @@ export const useAuththor = () => {
   const token = useRef<string | null>(null);
   const [error, setError] = useState<string | null>(null);
 
-  // Effect for setting the user based on the token
   useEffect(() => {
     if (typeof window !== 'undefined') {
-      const storedToken = localStorage.getItem('token');
-      token.current = storedToken;
-      if (storedToken) {
+      token.current = localStorage.getItem('token');
+      if (token.current) {
         try {
-          const decodedUser = jwtDecode<JwtUserProps>(storedToken); // Ensure jwtDecode is correctly typed
-          setCurrentUser(decodedUser);
-        } catch (err) {
-          setError('Failed to decode token');
+          const decodeToken = jwtDecode<JwtUserProps>(token.current);
+          setCurrentUser(decodeToken);
+          setIsAdmin(decodeToken.role.some((role) => role.roleId === 1));
+        } catch (error) {
+          setError('Token is invalid');
         }
       }
     }
-  }, []); // Removed currentUser from dependencies to avoid infinite loop
-
-  // Separate effect for setting isAdmin based on currentUser changes
-  useEffect(() => {
-    if (currentUser && currentUser.role) {
-      const isAdmin = _.some(currentUser.role, { roleId: 1 });
-      setIsAdmin(isAdmin);
-    }
-  }, [currentUser]); // Depend only on currentUser
+  }, []);
 
   return { token: token.current, currentUser, isAdmin, error };
 };
